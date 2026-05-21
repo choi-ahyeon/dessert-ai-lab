039b77bdd1f03d3e435f9fe3a6be909e1ecc3176
cjw4108 <chlwldnjs4108@gmail.com>
2026-05-21 10:14:12 +0900

    feat(api): validations fruitmatch 엔드포인트 추가

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://renovator-rubber-pony.ngrok-free.dev"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
