7a857c18ddc6d8a1a413ba439cdd4b56258a6030
cjw4108 <chlwldnjs4108@gmail.com>
2026-05-21 09:27:20 +0900

    feat(next): API URL 설정 및 CORS 업데이트

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    allowedDevOrigins: ['renovator-rubber-pony.ngrok-free.dev'],
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:8000/:path*',
+            },
+        ]
+    },
+}
 
-export default nextConfig;
+export default nextConfig
